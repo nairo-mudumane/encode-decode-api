acdf1b5c71d859d16cb40cbcf037cd8eb745f6e3
nairo-mudumane <nairomudumane@outlook.com>
2022-09-08 09:55:39 +0200

    fixed not found route

--- src/routes/notFound.js
+++ src/routes/notFound.js
@@ -1,5 +1,5 @@
 const notFound = (app) => {
-  app.all("/", (req, res) => {
+  app.all("*", (req, res) => {
     return res.status(404).json({ error: true, message: "not found" });
   });
   return;
